19a7cabc5280be638f197cf8f9da98460c5dfa76
Thiago <thiagooffice2@gmail.com>
2023-08-03 20:33:14 -0300

    feat: arrays

---
+++ arrays/lista.rb
@@ -0,0 +1,12 @@
+lista = []
+
+lista.push("thiago", "João", "Fernanda", "Pedro")
+
+lista << "Maria"
+
+lista.insert(0, "Fulano")
+lista.insert(2, "Ciclano")
+
+lista_organizada = lista.sort
+
+puts lista_organizada.last
