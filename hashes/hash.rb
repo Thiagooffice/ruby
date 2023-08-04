f66faab4c529a76d355263be9c271b4791fd7e72
Thiago <thiagooffice2@gmail.com>
2023-08-03 21:08:04 -0300

    feat: hashes

---
+++ hashes/hash.rb
@@ -0,0 +1,16 @@
+hash = {
+    nome: "Thiago",
+    idade: 23
+}
+hash[:altura] = 1.73
+# hash.delete(:altura)
+puts hash.length
+# puts hash.size
+# puts hash.values
+# puts hash.has_key?(nome)
+# puts hash.clear
+
+# puts hash
+
+# hash1 = {um: 1, dois: 2, tres: 3}
+# hash2 = {um: 3, dois: 4, tres: 5}
