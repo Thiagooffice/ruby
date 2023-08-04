3dcf9b39af5765dc583976eae4ec7fb2cb6f4cf9
Thiago <thiagooffice2@gmail.com>
2023-08-04 10:28:58 -0300

    feat: methods

---
+++ metodos/calculo.rb
@@ -0,0 +1,23 @@
+def soma(x, y)
+    x + y
+end
+
+def subt(x, y)
+    x - y
+end
+
+def mult(x, y)
+    x * y
+end
+
+def div(x, y)
+    x / y
+end
+
+puts soma 2, 4
+valorSubt = subt 5, 6
+if valorSubt <= 0
+    puts "é menor que 0"
+end
+puts mult 3, 9
+puts div 5, 10
