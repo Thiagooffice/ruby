e1c9a5ec71fdcf104505d2cf055507fb616d2d68
Thiago <thiagooffice2@gmail.com>
2023-08-03 22:28:42 -0300

    feat: repetitions

---
+++ repeticao/for.rb
@@ -0,0 +1,11 @@
+nomes = ["João", "maria", "josé", "mateus"]
+
+dict = {nome: "Diego", idade: 35, altura: 1.73}
+
+# for nome in nomes do
+#     puts nome
+# end
+
+for k, v in dict do
+    puts "#{k}: #{v}"
+end
---
+++ repeticao/times.rb
@@ -0,0 +1,6 @@
+c = 0
+2000.times do
+    puts "thiago"
+    puts c
+    c += 1
+end
