f8110d6e88e32d70660ef478f434c53832740aed
Thiago <thiagooffice2@gmail.com>
2023-08-04 10:53:07 -0300

    feat: ternary

---
+++ operadorTernario/ternario.rb
@@ -0,0 +1,12 @@
+nome = "t"
+
+# if nome.eql?("Thiago")
+#     puts "É o thiago"
+# else
+#     puts "Visitante."
+# end
+
+# condição ? true : false
+
+resultado = nome.eql?("Thiago") ? "É o thiago" : "Visitante."
+puts resultado
